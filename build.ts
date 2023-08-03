7d353750fed19da18d04cad39b576732839e74ec
nobigit <nobigit@users.noreply.github.com>
2023-08-03 21:26:44 +0900

    パッケージを作成

--- build.ts
+++ build.ts
@@ -1,11 +1,15 @@
+import * as pathLib from "path";
 import { execSync } from "child_process";
-import { copyFileSync, mkdirSync, readdirSync } from "fs";
+import { copyFileSync, createReadStream, createWriteStream, mkdirSync, readdirSync, statSync } from "fs";
+const archiver = require('archiver');
 
 const DEST = './dist';
 const SRC = './src';
+const ZIP = './pack.zip';
 
 compileTS();
 copyFiles();
+zip();
 
 function copyFiles() {
     for (const f of readdirSync('./src')) {
@@ -22,4 +26,44 @@ function copyFiles() {
 function compileTS() {
     const msg = execSync('npx tsc --build --verbose', { 'encoding': 'ascii' });
     console.log(msg);
+}
+
+function zip() {
+    console.log('Packaging...');
+    const output = createWriteStream(ZIP);
+    const archive = archiver('zip', {
+        zlib: { level: 9 }
+    });
+    archive.on('warning', (err: any) => {
+        if (err.code === 'ENOENT') {
+            console.log('file no entry.');
+        } else {
+            console.error(err);
+            throw err;
+        }
+    });
+    archive.on('error', (err: any) => {
+        console.error(err);
+        throw err;
+    });
+    archive.pipe(output);
+    for (const p of enumFiles(DEST)) {
+        archive.append(createReadStream(pathLib.join(DEST, p)), { name: p });
+    }
+    archive.finalize();
+    console.log(`Packaging done`);
+}
+
+function enumFiles(dir: string): string[] {
+    const files: string[] = [];
+    for (const entry of readdirSync(dir)) {
+        const p = pathLib.join(dir, entry);
+        const stat = statSync(p);
+        if (stat.isFile()) {
+            files.push(entry);
+        } else if (stat.isDirectory()) {
+            files.push(...enumFiles(p).map(f => pathLib.join(entry, f)));
+        }
+    }
+    return files;
 }
